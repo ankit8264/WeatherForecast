8e1bd7f3a530c4a086f9e76e53521c1ce57713d4
Ankit <ankit9801434282@gmail.com>
2023-01-16 14:19:38 +0530

    Testing

--- WeatherReport/Controllers/WeatherForecastController.cs
+++ WeatherReport/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WeatherReport.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Testing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Testing", "Extenda", "Retail", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
